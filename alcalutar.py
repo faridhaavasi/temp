084b0327bb37af2476d727dc30da411c9e6479f3
faridhavasi <faridhavasi@yahoo.com>
2022-05-20 00:29:02 +0430

    fun:0

---
+++ alcalutar.py
@@ -0,0 +1,19 @@
+from urllib import response
+
+
+oper=input('enter oper:')
+num1=float(input('enter nim1:'))
+num2=float(input('num2:'))
+response=float(response)
+if oper =="+":
+    reponse=num1+num2
+elif oper =="-":
+    reponse= num1-num2
+elif oper =="*":
+    response=num1*num2
+elif oper =="/":
+    response=num1/num2
+else:
+    pass
+
+
