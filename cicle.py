9ddd9de22937cf17c2ad5664468785e0ae663fdc
faridhavasi <faridhavasi@yahoo.com>
2022-05-27 15:04:54 +0430

    circl class

---
+++ cicle.py
@@ -0,0 +1,14 @@
+class Circle:
+
+    def __init__(self,r:float):
+        self.r=r
+        self.pi=3.14
+    def getArea(self):
+        a=self.pi*self.r**2
+        return float(a)
+    def getPerimeter(self):
+        p= 2 * self.pi*self.r
+        return p
+new_cir=Circle(11)
+print(new_cir.getArea())
+print(new_cir.getPerimeter())
