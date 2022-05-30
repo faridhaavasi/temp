add6121badce8d30540921c4f420fa2b891e003e
faridhavasi <faridhavasi@yahoo.com>
2022-05-30 12:26:08 +0430

    log

---
+++ log.py
@@ -0,0 +1,9 @@
+import math 
+def logarithm(n,m):
+    print(math.log(m,n))
+logarithm(5, 25) #➞ 2
+
+logarithm(2, 64) #➞ 6
+
+logarithm(2, 4) #➞ 2 
+# lin k of task: https://backendbaz.ir/practice/4379/   
